52ca1ab4d22fd65ce34e10a3057218107f609632
luiz <mollgoetten3012lpg@gmail.com>
2024-12-11 12:55:14 -0300

    creating project and initial components of main page

---
+++ src/components/step/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+import { colors } from "@/styles/theme"
+import { s } from "./styles";
+
+export type Props = {
+  title: string
+  description: string
+}
+
+export function Step({ title, description }: Props){
+  return(
+    <View style={s.container}>
+      <View style={s.details}>
+        <Text style={s.title}>{title}</Text>
+        <Text style={s.description}>{description}</Text>
+      </View>
+    </View>
+  )
+}
